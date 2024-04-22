ec47f36a74d6c32c26ec0ca4f67bb0cf902a6b5b
Clement <clement.roelens@hotmail.com>
2024-04-22 10:02:29 +0200

    conception finie

---
+++ back-tp-final/src/test/java/org/example/backtpfinal/BackTpFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backtpfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTpFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
