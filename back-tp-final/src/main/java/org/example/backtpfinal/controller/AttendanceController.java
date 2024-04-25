f310f2b4014f3bce407367da4bd664e210dc8fc2
jounetsuST <sanhtam.lam@gmail.com>
2024-04-25 07:13:10 +0200

    :app: calcul hours

--- back-tp-final/src/main/java/org/example/backtpfinal/controller/AttendanceController.java
+++ back-tp-final/src/main/java/org/example/backtpfinal/controller/AttendanceController.java
@@ -1,24 +1,37 @@
 package org.example.backtpfinal.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.backtpfinal.dto.AttendanceDTO;
+
 import org.example.backtpfinal.entities.Attendance;
+import org.example.backtpfinal.entities.Employee;
+import org.example.backtpfinal.exception.EmployeeNotFound;
+import org.example.backtpfinal.repository.AttendanceRepository;
 import org.example.backtpfinal.service.AttendanceService;
 import org.example.backtpfinal.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.*;
 
 
 @RestController
 @RequestMapping("/api/attendance")
+@CrossOrigin(origins = "*")
+@Slf4j
 public class AttendanceController {
     @Autowired
     private  AttendanceService attendanceService;
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private AttendanceRepository attendanceRepository;
+
 
     @PostMapping("/clockIn")
     public ResponseEntity<LocalDateTime> clockIn(@RequestBody AttendanceDTO dto) {
@@ -36,12 +49,35 @@ public class AttendanceController {
         System.out.println("Hoop out!!! " + endTime);
         return ResponseEntity.ok(endTime);
     }
-   /* @GetMapping("/getHours/{id}")
-    public ResponseEntity<Double> getHoursWorked(@PathVariable Long id) throws IllegalArgumentException{
-        Attendance attendance = (Attendance) attendanceService.getAttendanceFromSelectedDate();
-        double hours = attendanceService.hoursWorkedDaily(attendance);
-        return ResponseEntity.ok(hours);
+
+    @GetMapping("/hours-day/{employeeId}") //http://localhost:8080/api/attendance/hours-day/{employeeId}?date={date}
+    public ResponseEntity<Double> calculateHoursWorkedForDay(
+                @PathVariable Long employeeId, @RequestParam LocalDate date) {
+            Employee employee = employeeService.getById(employeeId)
+                    .orElseThrow(() -> new EmployeeNotFound(employeeId));
+
+            double hoursWorked = attendanceService.calculateHoursWorkedByEmployeeForDay(date, employee);
+
+            return ResponseEntity.ok(hoursWorked);
+
     }
-*/
+    @GetMapping("/{employeeId}/overtime") //http://localhost:8090/api/attendance/{id}/overtime
+    public ResponseEntity<Duration> getOvertimeForWeek(@PathVariable Long employeeId) {
+        Duration overtime = attendanceService.overtimeByWeek(employeeId);
+        return ResponseEntity.ok(overtime);
+    }
+
+    @GetMapping("week/{employeeId}")//http://localhost:8090/api/attendance/week/{id}?startDate=2024-04-25&endDate=2024-04-30
+    public ResponseEntity<Double> getEmployeeHoursForWeek(@PathVariable Long employeeId, @RequestParam LocalDateTime startDate, @RequestParam LocalDateTime endDate) throws EmployeeNotFound {
+       
+        Employee employee = employeeService.getById(employeeId)
+                .orElseThrow(() -> new EmployeeNotFound(employeeId));
+
+        Duration hoursByEmployeeForWeek = attendanceService.calculateHoursWorkedByEmployeeForWeek(startDate, endDate, employee);
+        double totalHours = hoursByEmployeeForWeek.toMinutes() / 60.0;
+
+        return ResponseEntity.ok(totalHours);
+    }
+
 
 }
